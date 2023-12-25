2b59e40402c7ba75c975acf96824e16ac77993da
7shi <7shi@live.jp>
2023-12-26 03:21:17 +0900

    try to fix errors

---
+++ scripts/word-fix.py
@@ -0,0 +1,61 @@
+import common, os, re
+
+error_qs = common.read_queries("error.xml")
+errors = {q.info: q for q in error_qs}
+
+# for q in error_qs:
+#     print(q.info, q.error.split("\n")[0])
+
+fix = {}
+for f in os.listdir("."):
+    if re.match(r"fix-\d+\.xml", f):
+        for q in common.read_queries(f):
+            if q.result:
+                fix[q.info] = q
+
+# sys.exit(0)
+
+init_qs = common.read_queries("init.xml")
+history = common.unzip(init_qs)
+# init_prompt = "Create a word table.\n" + "\n".join(history[2].split("\n")[-4:])
+
+import gemini
+
+def init():
+    gemini.init(history, [init_qs[2].prompt])
+
+qs = []
+
+# init()
+# for i in ["[Inferno Canto 13] 127/151"]:
+#     q = errors[i]
+#     q = gemini.query(q.prompt, q.info, show=True, retry=False)
+#     if q.result:
+#         qs.append(q)
+
+count = 0
+fixed = 0
+for q in error_qs:
+    if q.info in fix:
+        continue
+    lines = q.prompt.split("\n")
+    for i, line in enumerate(lines[1:]):
+        if count % 30 == 0:
+            init()
+        count += 1
+        qq = gemini.query(f"{lines[0]}\n{line}", f"{q.info}+{i}", show=True, retry=False)
+        qs.append(qq)
+        if qq.result:
+            fixed += 1
+
+print("Fixed:", fixed, "/", count)
+
+if qs:
+    prefix = "fix-" if fixed else "error-"
+    num = 1
+    for f in os.listdir("."):
+        if m := re.match(prefix + r"-(\d+)\.xml", f):
+            num = max(num, int(m.group(1)))
+    fn = f"{prefix}-{num+1:03}.xml"
+    common.write_queries(fn, qs, fixed=fixed, count=len(qs))
+    print("Written:", fn)
