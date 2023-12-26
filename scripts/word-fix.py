4a44946dddd8c3c30dd9ddc486e7eb847fb9341d
7shi <7shi@live.jp>
2023-12-26 18:52:21 +0900

    fix errors in word

--- scripts/word-fix.py
+++ scripts/word-fix.py
@@ -1,6 +1,6 @@
 import common, os, re
 
-error_qs = common.read_queries("error.xml")
+error_qs = common.read_queries("1-error.xml")
 errors = {q.info: q for q in error_qs}
 
 # for q in error_qs:
@@ -8,7 +8,7 @@ errors = {q.info: q for q in error_qs}
 
 fix = {}
 for f in os.listdir("."):
-    if re.match(r"fix-\d+\.xml", f):
+    if re.match(r".*fix.*\.xml", f):
         for q in common.read_queries(f):
             if q.result:
                 fix[q.info] = q
@@ -33,17 +33,31 @@ qs = []
 #     if q.result:
 #         qs.append(q)
 
+one_line = True
+
 count = 0
 fixed = 0
 for q in error_qs:
     if q.info in fix:
         continue
-    lines = q.prompt.split("\n")
-    for i, line in enumerate(lines[1:]):
+    if one_line:
+        lines = q.prompt.split("\n")
+        for i, line in enumerate(lines[1:]):
+            info = f"{q.info}+{i}"
+            if info in fix:
+                continue
+            if count % 30 == 0:
+                init()
+            count += 1
+            qq = gemini.query(f"{lines[0]}\n{line}", info, show=True, retry=False)
+            qs.append(qq)
+            if qq.result:
+                fixed += 1
+    else:
         if count % 30 == 0:
             init()
         count += 1
-        qq = gemini.query(f"{lines[0]}\n{line}", f"{q.info}+{i}", show=True, retry=False)
+        qq = gemini.query(q.prompt, q.info, show=True, retry=False)
         qs.append(qq)
         if qq.result:
             fixed += 1
@@ -51,8 +65,8 @@ for q in error_qs:
 print("Fixed:", fixed, "/", count)
 
 if qs:
-    prefix = "fix-" if fixed else "error-"
-    num = 1
+    prefix = "fix" if fixed else "error"
+    num = 0
     for f in os.listdir("."):
         if m := re.match(prefix + r"-(\d+)\.xml", f):
             num = max(num, int(m.group(1)))
