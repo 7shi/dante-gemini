498dbefd29351d4f9c8a16369572b3f80b46f195
7shi <7shi@live.jp>
2024-01-01 12:56:32 +0900

    support multiple languages and options

--- scripts/word-tr.py
+++ scripts/word-tr.py
@@ -2,14 +2,31 @@ import sys, os, re, common
 
 args = sys.argv[1:]
 
-directories = "inferno purgatorio paradiso"
+directories = common.directories
+translate = ["English", "Italian"]
+init_xml = "init.xml"
+fields = [1]
+interval = 10
 once  = False
 show  = True
 retry = True
 i = 0
 while i < len(args):
     if args[i] == "-d" and len(args) > i + 1:
-        directories = args.pop(i + 1)
+        directories = args.pop(i + 1).split()
+        args.pop(i)
+    elif args[i] == "-t" and len(args) > i + 1:
+        translate = [l.strip() for l in args.pop(i + 1).split(",")]
+        args.pop(i)
+    elif args[i] == "-f" and len(args) > i + 1:
+        tmp = args.pop(i + 1).strip()
+        fields = [int(f) for f in tmp.split(",")] if tmp else []
+        args.pop(i)
+    elif args[i] == "-i" and len(args) > i + 1:
+        init_xml = args.pop(i + 1)
+        args.pop(i)
+    elif args[i] == "-n" and len(args) > i + 1:
+        interval = int(args.pop(i + 1))
         args.pop(i)
     elif args[i] == "-1":
         once = True
@@ -25,6 +42,10 @@ while i < len(args):
 
 if len(args) < 3:
     print(f"Usage: python {sys.argv[0]} language word-dir output-dir [fix ...]", file=sys.stderr)
+    print("  -t: specify language to translate (comma separated)", file=sys.stderr)
+    print("  -f: specify field to column 2 (0-based, comma separated)", file=sys.stderr)
+    print("  -i: specify init.xml", file=sys.stderr)
+    print("  -n: specify interval (default 10)", file=sys.stderr)
     print("  -d: specify sub directory", file=sys.stderr)
     print("  -1: just do one canto", file=sys.stderr)
     print("  --no-retry: don't retry queries", file=sys.stderr)
@@ -39,7 +60,10 @@ fixes = common.read_fixes(*fix_files)
 import gemini
 
 def send(query):
-    prompt = "For each row in the table, fill the third column with a literal translation of the first column."
+    global fields
+    colstart = 3 if fields else 2
+    colnum = ", ".join(str(colstart + c) for c in range(len(translate)))
+    prompt = f"For each row in the table, fill in columns {colnum} with the direct translation of column 1."
     if not query.result:
         q = common.query()
         q.info = query.info
@@ -49,28 +73,43 @@ def send(query):
     table = []
     for i, row in enumerate(common.read_table(query.result)):
         if i == 0:
-            table.append(f"| {language} | Lemma | English |")
+            head = " | " + " ".join(row[f] for f in fields) if fields else ""
+            table.append(f"| {language}{head} | " + " | ".join(translate) + " |")
         elif i == 1:
-            table.append("|" + "|".join(row[:3]) + "|")
+            table.append("|---" * ((2 if fields else 1) + len(translate)) + "|")
         else:
-            lemma = row[1]
-            if lemma:
-                table.append(f"| {row[0]} | {lemma} | |")
+            lemma = " | " + " ".join(row[f] for f in fields) if fields else ""
+            lu = (lemma if lemma else row[0]).strip().upper()
+            if lu != lu.lower():
+                table.append(f"| {row[0]}{lemma} |" + " |" * len(translate))
     prompt += "\n\n"
     prompt += "\n".join(table)
     return gemini.query(prompt, query.info, show, retry)
 
-for directory in directories.split():
-    path = os.path.join(outdir, directory)
-    if not os.path.exists(path):
-        os.mkdir(path)
+if os.path.exists(init_xml):
+    init_qs = common.read_queries(init_xml)
+else:
+    print(f"making {init_xml}...")
+    gemini.init()
+    inferno1 = common.read_queries(os.path.join(worddir, "inferno", "01.xml"))
+    init_qs = [send(inferno1[0])]
+    common.write_queries(init_xml, init_qs, count=len(init_qs))
+history = common.unzip(init_qs)
+
+for directory in directories:
+    worddir2 = os.path.join(worddir, directory)
+    if not os.path.exists(worddir2):
+        continue
+    outdir2 = os.path.join(outdir, directory)
+    if not os.path.exists(outdir2):
+        os.mkdir(outdir2)
     files = [
-        (int(m.group(1)), os.path.join(worddir, directory, f))
-        for f in sorted(os.listdir(os.path.join(worddir, directory)))
+        (int(m.group(1)), os.path.join(worddir2, f))
+        for f in sorted(os.listdir(worddir2))
         if (m := re.match(r"(\d+)\.xml", f))
     ]
     for canto, file in files:
-        xml = os.path.join(path, f"{canto:02}.xml")
+        xml = os.path.join(outdir2, f"{canto:02}.xml")
         if os.path.exists(xml):
             continue
         print()
@@ -78,8 +117,8 @@ for directory in directories.split():
         queries = common.read_queries(file)
         qs = []
         for query in queries:
-            if len(qs) % 10 == 0:
-                gemini.init()
+            if len(qs) % interval == 0:
+                gemini.init(history)
             if not query.result and query.info in fixes:
                 for q in fixes[query.info]:
                     qs.append(send(q))
