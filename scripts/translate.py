270e4f06ebdc56ce5f0415b0fd838c65fc9ac639
7shi <7shi@live.jp>
2024-01-14 22:12:56 +0900

    translate.py: support option -3

--- scripts/translate.py
+++ scripts/translate.py
@@ -1,16 +1,21 @@
 import sys, os, re, common, option
 
-needsp = False
+needsp  = False
+always3 = False
 
 def parse(i, args):
-    global needsp
+    global needsp, always3
     if args[i] == "--need-space":
         args.pop(i)
         needsp = True
+    elif args[i] == "-3":
+        args.pop(i)
+        always3 = True
 
 if not option.parse(parse) or option.args:
-    print(f"Usage: python {sys.argv[0]} language italian-dir output-dir", file=sys.stderr)
+    print(f"Usage: python {sys.argv[0]} language src-dir output-dir", file=sys.stderr)
     print("  --need-space: require at least one space in each line", file=sys.stderr)
+    print("  -3: always send 3 lines", file=sys.stderr)
     option.show()
     sys.exit(1)
 
@@ -18,18 +23,18 @@ checklen = 6 if " and " in option.language else 3
 
 import gemini
 
-it = []
+text = []
 current = 0
 
 def send_lines(line_count, *plines):
     global current
     diru = option.directory[0].upper() + option.directory[1:]
-    info = f"[{diru} Canto {option.canto}] {current + 1}/{len(it)}"
+    info = f"[{diru} Canto {option.canto}] {current + 1}/{len(text)}"
     prompt = " ".join(plines)
     for i in range(line_count):
         if i == 0 or current % 3 == 0:
             prompt += "\n"
-        line = f"{current + 1} {it[current]}"
+        line = f"{current + 1} {text[current]}"
         prompt += "\n" + line
         current += 1
     def check(r):
@@ -38,8 +43,8 @@ def send_lines(line_count, *plines):
         if needsp:
             for line in r.split("\n"):
                 if m := re.match(r"(\d+)", line):
-                    text = line[m.end():]
-                    if not text.startswith(" ") or " " not in text[1:]:
+                    t = line[m.end():]
+                    if not t.startswith(" ") or " " not in t[1:]:
                         return f"Too few spaces: {repr(r)}"
         return None
     return gemini.query(prompt, info, option.show, option.retry, check)
@@ -54,9 +59,9 @@ else:
     option.canto = 1
     file = os.path.join(option.srcdir, option.directory, f"01.txt")
     with open(file, "r", encoding="utf-8") as f:
-        it = [l for line in f if (l := line.strip())]
+        text = [l for line in f if (l := line.strip())]
     init_qs = []
-    for length in [3, 6]:
+    for length in [3, 3, 3] if always3 else [3, 6]:
         q = send_lines(length, prompt)
         if not q.result:
             print("Abort.", file=sys.stderr)
@@ -68,16 +73,17 @@ init_ps = history[:1] if option.interval == 1 else None
 
 @option.proc
 def proc(src, xml):
-    global it, current
+    global text, current
     with open(src, "r", encoding="utf-8") as f:
-        it = [l for line in f if (l := line.strip())]
+        text = [l for line in f if (l := line.strip())]
     current = 0
     qs = []
-    while current < len(it):
+    while current < len(text):
         if not (0 <= gemini.chat_count < option.interval):
             gemini.init(history, init_ps)
-        length = min(3, len(it) - current)
-        while current + length < len(it) and not it[current + length - 1].endswith("."):
-            length += 1
+        length = min(3, len(text) - current)
+        if not always3:
+            while current + length < len(text) and not text[current + length - 1].endswith("."):
+                length += 1
         qs.append(send_lines(length, prompt))
     common.write_queries(xml, qs, count=len(qs))
