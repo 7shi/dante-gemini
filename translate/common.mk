f9772d4caf5fd7eb4ddcc13c9e897e254b62d9ac
7shi <7shi@live.jp>
2024-01-18 02:25:32 +0900

    translate/common.mk: add targets

--- translate/common.mk
+++ translate/common.mk
@@ -1,6 +1,7 @@
 TOPDIR  ?= ../..
 SCRIPTS  = $(TOPDIR)/scripts
 SRCDIR   = $(TOPDIR)/it
+DIRSM    = {inferno,purgatorio,paradiso}
 OPTIONS ?=
 
 all: run check
@@ -9,7 +10,19 @@ run:
 	python $(SCRIPTS)/translate.py $(OPTIONS) "$(LANG)" $(SRCDIR) .
 
 check:
-	python $(SCRIPTS)/pickup.py 1-error.xml */*.xml
+	python $(SCRIPTS)/pickup.py 1-error.xml $(DIRSM)/*.xml
 
 fix:
 	python $(SCRIPTS)/word-fix.py
+
+split:
+	python $(SCRIPTS)/split3.py $(DIRSM)/*.xml
+
+redo:
+	python $(SCRIPTS)/redo.py 1-error.xml
+
+redo1:
+	python $(SCRIPTS)/redo.py -1 1-error.xml
+
+replace:
+	python $(SCRIPTS)/replace.py 1-error-ok.xml $(DIRSM)/*.xml
